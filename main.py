809d89490b0544018e5e3fbc9a7b8600645834f0
Lautaro Barba <lau_mb94@hotmail.com>
2022-11-04 22:20:04 -0300

    Se movieron las salidas a una nueva carpeta

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import datetime
 from PIL import Image
 
 img = Image.open('imagen.png')
 
 grados = int(input('Ingrese la cantidad de grados: '))
 out = img.rotate(grados)
-out.save('rotada.png')
+
+# creo una hora para agregarla al nombre
+hora = datetime.datetime.now().strftime("%H-%M-%S-%p")
+out.save('salidas/rotada-'+hora+'.png')
